6424e7b0a3f07183b9dd7852a9bb1fb3694ff59e
Programer-akram <mah.akram202@gmail.com>
2022-08-30 02:29:09 +0600

    button in control

--- js/index.js
+++ js/index.js
@@ -1,1 +1,112 @@
-console.log('hlw from js');
+
+let nodelist = 0;
+document.getElementById('btn-messi').addEventListener('click',function(){
+
+    const button = document.getElementById('btn-messi');
+    button.setAttribute('disabled', true);
+    nodelist++;
+    if (nodelist > 5) {
+        alert('Can not add more than Five!!');
+    return;
+        }
+    const selecting = document.getElementById('selected');
+    const li1 = document.createElement('li');
+    li1.innerText = document.getElementById('messi').innerText;
+    selecting.appendChild(li1);
+  });
+   
+
+document.getElementById('btn-neymar').addEventListener('click',function(){
+    const button = document.getElementById('btn-neymar');
+    button.setAttribute('disabled', true);
+
+    nodelist++;
+    if (nodelist > 5) {
+        alert('Can not add more than Five!!');
+
+        return;
+
+    }
+   const selecting = document.getElementById('selected');
+   const li1 = document.createElement('li');
+    li1.innerText = document.getElementById('neymar').innerText;
+    selecting.appendChild(li1);
+    });
+   
+document.getElementById('btn-ronaldo').addEventListener('click',function(){
+    const button = document.getElementById('btn-ronaldo');
+    button.setAttribute('disabled', true);
+
+    nodelist++;
+    if (nodelist > 5) {
+        alert('Can not add more than Five!!');
+
+        return;
+}
+    const selecting = document.getElementById('selected');
+    const li1 = document.createElement('li');
+    li1.innerText = document.getElementById('ronaldo').innerText;
+    selecting.appendChild(li1);
+ });
+
+
+   
+document.getElementById('btn-meradona').addEventListener('click',function(){
+    const button = document.getElementById('btn-meradona');
+    button.setAttribute('disabled', true);
+
+    nodelist++;
+    if (nodelist > 5) {
+        alert('Can not add more than Five!!');
+
+        return;
+ }
+    const selecting = document.getElementById('selected');
+    const li1 = document.createElement('li');
+    li1.innerText = document.getElementById('meradona').innerText;
+    selecting.appendChild(li1);
+   });
+
+document.getElementById('btn-kaka').addEventListener('click',function(){
+    const button = document.getElementById('btn-kaka');
+    button.setAttribute('disabled', true);
+
+    nodelist++;
+    if (nodelist > 5) {
+        alert('Can not add more than Five!!');
+
+        return;
+    }
+    
+    const selecting = document.getElementById('selected');
+    const li1 = document.createElement('li');
+    li1.innerText = document.getElementById('kaka').innerText;
+    selecting.appendChild(li1);
+ });
+   
+document.getElementById('btn-pele').addEventListener('click',function(){
+
+    const button = document.getElementById('btn-pele');
+    button.setAttribute('disabled', true);
+
+    nodelist++;
+    if (nodelist > 5) {
+        alert('Can not add more than Five!!');
+
+        return;
+ }
+
+   const selecting = document.getElementById('selected');
+   const li1 = document.createElement('li');
+    li1.innerText = document.getElementById('pele').innerText;
+    selecting.appendChild(li1);
+});
+
+
+
+
+
+
+
+
+
